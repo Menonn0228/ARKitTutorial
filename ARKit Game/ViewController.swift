4ec64bc06b5569639d442d97330e3b1e80b70937
Nikhil <menon.nikhil@icloud.com>
2020-02-10 12:21:28 -0600

    Switched to Programmatic UI.

--- ARKit Game/ViewController.swift
+++ ARKit Game/ViewController.swift
@@ -12,28 +12,70 @@ import ARKit
 class ViewController: UIViewController {
 
     // MARK: - Subviews
+    let sceneView = ARSCNView()
     
-    @IBOutlet weak var sceneView: ARSCNView!
-    @IBOutlet weak var counterLabel: UILabel!
+    let scoreLabel:UILabel = {
+       let label = UILabel()
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.font = .boldSystemFont(ofSize: 20)
+        label.backgroundColor = .white
+        label.numberOfLines = 0
+        label.text = "0"
+        return label
+    }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    // MARK: Subview Constraints
+    /// Sets the constraints for subviews
+    public func setSubviewConstraints() {
+        view.addSubview(sceneView)
+        view.addSubview(scoreLabel)
+        sceneView.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
         
+        NSLayoutConstraint.activate([
+            
+            // scoreLabel Constraints
+            scoreLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scoreLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scoreLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scoreLabel.heightAnchor.constraint(equalToConstant: 70),
+            
+            // sceneView Constraints
+            sceneView.topAnchor.constraint(equalTo: view.topAnchor),
+            sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            sceneView.bottomAnchor.constraint(equalTo: scoreLabel.topAnchor)
+        ])
+    }
+    
+    // MARK: - Configure Scene
+    /// Configures the SceneView
+    private func configureScene() {
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    /// Sets the scene for viewDidLoad
+    private func setScene() {
         let scene = SCNScene()
         sceneView.scene = scene
     }
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Call constaint methods
+        setSubviewConstraints()
+        
+        // Configure the Scene View
+        setScene()
+    }
+    
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        let configuration = ARWorldTrackingConfiguration()
-        
-        sceneView.session.run(configuration)
-        
-        
-        
-        
+        configureScene()
     }
 
 
